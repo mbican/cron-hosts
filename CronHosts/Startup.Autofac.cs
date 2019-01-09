cd128f3a43ecec9ac412b5534acce2401432aec3
Michal Bičan <bicanm@gmail.com>
2019-01-09 20:35:32 +0100

    CH-0: Create Hello World Console application with autofac

---
+++ CronHosts/Startup.Autofac.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace CronHosts
+{
+    public static partial class Startup
+    {
+        public static void ConfigureAutofac(ContainerBuilder builder)
+        {
+        }
+    }
+}
---
+++ CronHosts/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace CronHosts
+{
+    public static partial class Startup
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+            ConfigureAutofac(builder);
+            return builder.Build();
+        }
+    }
+}
